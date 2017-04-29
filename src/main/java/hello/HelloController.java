f772182976517b70cf176b9889faf437418d919b
TuanKiet2309 <nguyentuankiet0996@gmail.com>
2017-04-29 10:33:03 +0700

    test

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class HelloController {
     
-    @GetMapping("/")
-    public String index() {
+	@RequestMapping(value = { "/test" }, method = RequestMethod.GET)
+    public String test() {
         return "uploadForm";
+    }
+	
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+    public String index() {
+        return "index";
     }
     
 }
