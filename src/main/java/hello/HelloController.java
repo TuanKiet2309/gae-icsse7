95d6ec42428385759b0c7f521defe16bfd7c1f29
TuanKiet2309 <nguyentuankiet0996@gmail.com>
2017-04-29 09:41:21 +0700

    test

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,16 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
     
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "uploadForm";
     }
     
 }
